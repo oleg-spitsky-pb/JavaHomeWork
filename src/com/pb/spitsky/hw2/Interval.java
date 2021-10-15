a518cf20b994a7a097347798211b8a0108af9ce0
Oleg Spitsky <oleg.spitsky@privatbank.ua>
2021-10-15 23:45:04 +0300

    init commit

--- src/com/pb/spitsky/hw2/Interval.java
+++ src/com/pb/spitsky/hw2/Interval.java
@@ -1,12 +1,12 @@
 package com.pb.spitsky.hw2;
 import java.util.Scanner;
-public class intarval {
+class interval {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int operand;
         System.out.print("Введите, пожалуйста, число от 0 до 100:");
         operand = scan.nextInt();
-        if (operand == 0 && operand <=14) {
+        if (operand >= 0 && operand <=14) {
             System.out.print("Диапазон от 0 до 14");
         } else if (operand >= 15 && operand <= 35) {
             System.out.print("Диапазон от 15 до 35");
